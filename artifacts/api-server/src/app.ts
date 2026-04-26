65d803734e3f6acf7229a0624d737dac66bc1158
Webdeveloper-A <arabboyabdurasulov65@gmail.com>
2026-04-26 23:39:53 +0500

    Fix frontend Vercel build config

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { type Request, type Response } from "express";
+﻿import express, { type Request, type Response } from "express";
 import { createRequire } from "node:module";
 import routes from "./routes";
 
